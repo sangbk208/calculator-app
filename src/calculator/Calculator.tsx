f2246488d4be443d8ca3d75de988152a9148ca19
sangbk208 <sangbk208@gmail.com>
2022-10-01 05:17:53 +0700

    integrate the useCalculator hook to Calculator CPN

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -1,13 +1,56 @@
 import React from "react";
 import { NumericKeypad } from "../components/NumericKeypad";
 import { Screen } from "../components/Screen/Screen";
+import { useCalculator } from "../hooks/calculator";
 import "./Calculator.css";
 
 export const Calculator = () => {
+  const {
+    operator,
+    displayValue,
+    inputEqual,
+    inputNumber,
+    inputOperator,
+    inputDot,
+    toggleSign,
+    clearAll,
+  } = useCalculator();
+
+  const onClickKeypad = (key: string) => {
+    switch (key) {
+      case "C":
+        clearAll();
+        break;
+      case "=":
+        inputEqual();
+        break;
+      case "+":
+      case "-":
+      case "x":
+      case "/":
+        inputOperator(key);
+        break;
+      case "±":
+        toggleSign();
+        break;
+      case ".":
+        inputDot();
+        break;
+      case "H":
+        break;
+      default:
+        inputNumber(key);
+    }
+  };
+
   return (
     <div>
-      <Screen />
-      <NumericKeypad />
+      <Screen className="calculator-screen" displayValue={displayValue} />
+      <NumericKeypad
+        handleOnClick={onClickKeypad}
+        operator={operator}
+        isClearAvailable={displayValue !== "0" || operator !== ""}
+      />
     </div>
   );
 };
