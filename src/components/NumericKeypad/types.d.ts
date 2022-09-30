dec7caddb35e4a71fe4b4321a05a017d2aef8050
sangbk208 <sangbk208@gmail.com>
2022-09-30 17:47:33 +0700

    rename operation button

--- src/components/NumericKeypad/types.d.ts
+++ src/components/NumericKeypad/types.d.ts
@@ -1,4 +1,8 @@
 export interface IButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  type?: "default" | "number" | "calculation";
+  type?: "default" | "number" | "operation";
+}
+
+export interface INumericKeypad {
+  handleOnClick: (value: string) => void;
 }
