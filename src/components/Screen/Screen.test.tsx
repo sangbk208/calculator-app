70401112f15a335c23b003a95a2bce5fef5a586a
sangbk208 <sangbk208@gmail.com>
2022-10-01 18:17:05 +0700

    Unit test for Screen CPN

---
+++ src/components/Screen/Screen.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { Screen } from "./Screen";
+
+describe("<Screen />", () => {
+  it("renders correctly", () => {
+    render(<Screen displayValue="1998" />);
+
+    const inputElement = screen.getByTestId("screen-input");
+
+    expect(screen.getByTestId("screen-container")).toBeInTheDocument();
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement).toHaveValue("1998");
+  });
+});
